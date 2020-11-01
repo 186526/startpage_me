5855d612efcf8bf26a625afdffbf05bd76eccf1c
186526 <i@186526.xyz>
2020-11-01 15:17:01 +0800

    Main

--- src/index.js
+++ src/index.js
@@ -1,5 +1,128 @@
-import { dns } from "./dns.js";
-import { terminal } from "./terminal.js";
-dns.doh_json_api("186526.xyz", "A", "119.29.29.29").then(e => { console.log(e); });
-let a = new terminal();
-a.mount("body");
+import { dns, dig } from "./web.js";
+import Term from 'term-web';
+var $ = function (a) {
+    return document.querySelector(a);
+};
+let terminal = {};
+terminal.term = new Term({
+    container: "#terminal-container",
+    pixelRatio: 2,
+    title: "Terminal at i@186526.xyz",
+    prefix: 'i@186526.xyz: ~ <d color="#00f501">$</d> ',
+    welcome: 'Welcome to <d color="#00f501">186526.xyz</d> terminal',
+    loading: () => 'Please Wait fot a moment……',
+    notFound: (val) => `<d color="#00f501">${val}</d> : Command not found`,
+    actions: [
+        {
+            input: /^dig/i,
+            output(input, args) {
+                if (input === "dig") {
+                    return "Usage: dig [+short] host [type]\n";
+                } else {
+                    if (args._.includes("+short")) {
+                        return dig(
+                            args._[2],
+                            ((b) => {
+                                if (typeof (b) == "undefined") {
+                                    return "A";
+                                }
+                                else {
+                                    return b;
+                                }
+                            })(args._[3]),
+                            true
+                        );
+                    } else {
+                        return dig(
+                            args._[1],
+                            ((b) => {
+                                if (typeof (b) == "undefined") {
+                                    return "A";
+                                } else {
+                                    return b;
+                                }
+                            })(args._[2]),
+                            false
+                        );
+                    }
+                }
+            }
+        },
+        {
+            input: /^curl/i,
+            async output(input, args) {
+                if (input === "curl") {
+                    return "curl url [GET/POST/PUT/DELETE,etc]";
+                } else {
+                    let a = await fetch(
+                        args._[1],
+                        ((b) => {
+                            if (typeof (b) == "undefined") {
+                                return { method: "GET" };
+                            } else {
+                                return { method: b };
+                            }
+                        })(args._[2])
+                        );
+                    return "<pre>"+await a.text()+"</pre>";
+                }
+            }
+        },
+        {
+            input: /^clear/i,
+            output() {
+                this.clear();
+                return "";
+            }
+        },
+        {
+            input: /^aboutme/i,
+            async output() {
+                let a = await fetch('./neofetch.output');
+                if (await a.status === 200) {
+                    let b = await a.text();
+                    return b;
+                } else {
+                    throw "Server Error";
+                }
+            }
+        },
+        {
+            input: /^friend/i,
+            async output() {
+                let a = await fetch('./friend.output');
+                if (await a.status === 200) {
+                    let b = await a.text();
+                    return b;
+                } else {
+                    throw "Server Error";
+                }
+            }
+        },
+        {
+            input: /^about/i,
+            output() {
+                let a = "";
+                a += "<d color='#50fa7b'>186526.xyz & i.186526.xyz</d>\n";
+                a += "Copyright © 19191 - 810 <d href='https://186526.xyz' color='yellow'>186526</d>\n";
+                a += "Host By <d href='https://vercel.com' color='yellow'>Vercel</d>\n";
+                a += "CDN Provider <d href='https://cloudflare.com' color='yellow'>Cloudflare</d>\n";
+                a += "Thank <d href='https://github.com/zhw2590582/term-web' color='yellow'>term-web</d>\n";
+                return a;
+            }
+        },
+        {
+            input: /^help/i,
+            output() {
+                let a = "";
+                a += "<d color='#50fa7b'>dig</d>:<d color='yellow'>dig - DNS lookup utility</d>\n";
+                a += "<d color='#50fa7b'>curl</d>:<d color='yellow'>curl - transfer a URL</d>\n";
+            }
+        }
+    ]
+});
+terminal.term.type("aboutme").then(() => {
+    terminal.term.type("friend").then(() => {
+        terminal.term.type("about");
+    })
+});
---
+++ src/web.js
@@ -0,0 +1,94 @@
+var dns = {
+    doh_json_api: async (
+        domain,
+        type = 1,
+        edns_subnet = null,
+        doh = "https://static.186526.top/doh/google/resolve"
+    ) => {
+        let list = [
+            { type: "A", id: 1 },
+            { type: "NS", id: 2 },
+            { type: "CNAME", id: 5 },
+            { type: "SOA", id: 6 },
+            { type: "TXT", id: 16 },
+            { type: "AAAA", id: 28 },
+        ];
+        try {
+            new URL(doh);
+        } catch (err) {
+            throw err;
+        }
+        let i = new URL(doh);
+        i.search = "name=" + domain;
+        if (typeof type === "number") {
+            i.search += "&type=" + type;
+        } else if (i.host !== "cloudflare-dns.com") {
+            for (let x in list) {
+                if (list[x].type === type) {
+                    i.search += "&type=" + list[x].id;
+                    break;
+                }
+            }
+        }
+        if (edns_subnet !== null) {
+            i.search += "&edns_client_subnet=" + edns_subnet;
+        }
+        let answer = await fetch((i.href), {
+            headers: {
+                accept: "application/dns-json",
+            },
+        });
+        return answer.json();
+    }
+};
+function digoutput(domain, results, type, time, short = false) {
+    let a = "";
+    if (short) {
+        a = ((results) => {
+            let a = "";
+            for (let i in results) {
+                a += `${results[i].data}\n`;
+            }
+            return a;
+        })(results);
+    } else {
+        a = `\n;<<>> Dig at Web 1.0.0 <<>> ${domain} ${type}\n`;
+        a += "\n";
+        a += ";; QUESTION SECTION:\n";
+        a += `;${domain}            IN  ${type}\n\n`;
+        a += ";; ANSWER SECTION:\n";
+        a += ((results, type, domain) => {
+            let a = "";
+            for (let i in results) {
+                a += `${domain}     ${results[i].TTL} IN ${type}        ${results[i].data}\n`;
+            }
+            return a;
+        })(results, type, domain);
+        a += `\n;; Query time: ${time} msec\n`;
+        a += `;; SERVER: https://static.186526.top/doh/google/resolve#443 at DOH\n`;
+        a += `;; WHEN: ${new Date()}\n\n`;
+    }
+    console.log(`[Get Command 'dig'] \n Output: \n${a}`);
+    return a;
+}
+async function dig(domain, type, short = false) {
+    const starttime = (Math.round(performance.now() * 100) / 100 / 1000).toFixed(3);
+    let a = dns.doh_json_api(
+        domain,
+        type = type
+    );
+    let b = await a;
+    if (await b.Status == 0) {
+        const endtime = (Math.round(performance.now() * 100) / 100 / 1000).toFixed(3);
+        return digoutput(
+            domain,
+            await b.Answer,
+            type,
+            (endtime - starttime).toFixed(3)*1000,
+            short
+        );
+    } else {
+        return "Domain not found";
+    }
+}
+export { dns, dig };
